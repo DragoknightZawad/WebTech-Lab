8227c3dc82779693d257598d3061a6c7007385db
DragoknightZawad <you@example.com>
2020-10-27 16:50:02 +0600

    1st part

---
+++ phptask.php
@@ -0,0 +1,120 @@
+<?php
+
+	$gender="";
+	$err_gender="";
+	$hobbies="";
+	$err_hobbies="";
+	$profession="";
+	$err_profession="";
+	$name="";
+	$err_name="";
+	
+	$has_error = false;
+	
+	if(isset($_POST["register"])){
+		if(empty($_POST["name"])){
+			$err_name="Name Required";
+			$has_error = true;
+		}elseif(strpos($_POST["name"]," ")){
+			$err_name="No space allowed";
+			$has_error = true;
+		}
+		else{
+			$name=htmlspecialchars($_POST["name"]);
+		}
+		if(!isset($_POST["gender"])){
+			$err_gender="Gender Required";
+			$has_error = true;
+		}
+		else{
+			$gender = $_POST["gender"];
+		}
+		if(!isset($_POST["hobbies"])){
+			$err_hobbies = "Atleast select 1 hobby";
+			$has_error = true;
+		}
+		else{
+			$hobbies=$_POST["hobbies"];
+		}
+		
+		if(!$has_error){
+			echo $name;
+		}
+		
+	}
+	
+?>
+<html>
+	<head>
+		<title>Club Member Registation</title>
+	</head>
+	<body>
+		
+		<hr>
+		<form action="" method="post">
+			<fieldset>
+				<legend><h1>Club Member Registation</h1></legend>
+				<table>
+					<tr>
+						<td>Name:</td>
+						<td><input type="text" name="name"> <?php echo $err_name;?></td>
+					</tr>
+					<tr>
+						<td>Username:</td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td>Password:</td>
+						<td><input type="password" name="password"></td>
+					</tr>
+					<tr>
+						<td>Confirm Password:</td>
+						<td><input type="password" name="password"></td>
+					</tr>
+					<tr>
+						<td>Email:</td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td>Adress:</td>
+						<td><input type="text"placeholder="Street Adress"></td>
+					</tr>
+						<tr>
+						<td></td>
+						<td><input type="text" placeholder="City" >-</td> 
+						<td><input type="text" placeholder="State" ></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><input type="text"placeholder="Postal/Zip Code"></td>
+					</tr>
+					
+					
+					
+					<tr>
+						<td>Gender: <?php echo $err_gender;?></td>
+						<td>
+							<input type="radio" name="gender" value="Male"> Male
+							<input type="radio" name="gender" value="Female"> Female
+						</td>
+					</tr>
+					<tr>
+						<td>Where did you hear about us  <?php echo $err_hobbies;?></td>
+						<td>
+							<input type="checkbox" name="hobbies[]" value=""> A Friend or Coligue<br>
+							<input type="checkbox" name="hobbies[]" value="Music"> Music <br>
+							<input type="checkbox" name="hobbies[]" value="Programming"> Programming<br>
+							<input type="checkbox" name="hobbies[]" value="Travelling"> Travelling <br>
+							<input type="checkbox" name="hobbies[]" value="Adventure"> Adventure<br>
+							<input type="checkbox" name="hobbies[]" value="Gradenning"> Gardenning<br>
+						</td>
+					</tr>
+				
+						<td colspan="2" align="center"><input type="submit" name="register" value="register"></td>
+					</tr>
+				</table>
+			</fieldset>
+		</form>
+	</body>
+	
+</html>
